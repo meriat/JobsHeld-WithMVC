000c9eab6ba54c3bed5d17abf04c52b77826d0d2
Guest User <Guest@epicodus-19.local>
2018-09-12 10:15:17 -0700

    Add cshtml files and model

---
+++ JobsHeld/Models/Job.cs
@@ -0,0 +1,37 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using JobsHeld.Models;
+
+namespace JobsHeld.Models
+{
+  public class Job
+  {
+    public string company {get; set;}
+    public string position {get; set;}
+    public string startDate {get; set;}
+    public string endDate {get; set;}
+    public string description {get; set;}
+    public int id {get;}
+    private static List<Job> _instances = new List<Job> {};
+
+    public Job (string newCompany, string newPosition, string newStartDate, string newEndDate, string newDescription)
+    {
+      company = newCompany;
+      position = newPosition;
+      startDate = newStartDate;
+      endDate = newEndDate;
+      description = newDescription;
+      _instances.Add(this);
+      id = _instances.Count;
+    }
+    public void Delete()
+    {
+      _instances.Remove(this);
+    }
+    public static Job Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
